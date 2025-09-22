76238ac6577211ece0a8719c81c075751db135b6
Lace <LoveLoveLowell@gmail.com>
2025-09-22 08:57:36 -0700

    awawawaw

---
+++ data/mods/parallelADV/formats-data.ts
@@ -0,0 +1,86 @@
+export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = {
+	dustbunny: {
+		tier: "OU",
+	},
+	notwhims: {
+		tier: "OU",
+	},
+	stadpersian: {
+		tier: "OU",
+	},
+	notgar: {
+		tier: "OU",
+	},
+	notskarm: {
+		tier: "OU",
+	},
+	rockheaddy: {
+		tier: "OU",
+	},
+	evil: {
+		tier: "OU",
+	},
+	notfero: {
+		tier: "OU",
+	},
+	notuxie: {
+		tier: "OU",
+	},
+	psykoro: {
+		tier: "OU",
+	},
+	flyingdark: {
+		tier: "OU",
+	},
+	notmag: {
+		tier: "OU",
+	},
+	notdug: {
+		tier: "OU",
+	},
+	sacredfirer: {
+		tier: "OU",
+	},
+	whaelstrom: {
+		tier: "OU",
+	},
+	notmew: {
+		tier: "OU",
+	},
+	levipoison: {
+		tier: "OU",
+	},
+	nottar: {
+		tier: "OU",
+	},
+	iceflyer: {
+		tier: "OU",
+	},
+	maid: {
+		tier: "OU",
+	},
+	ryubel: {
+		tier: "OU",
+	},
+	notmamo: {
+		tier: "OU",
+	},
+	napper: {
+		tier: "OU",
+	},
+	waterground: {
+		tier: "OU",
+	},
+	roughskinner: {
+		tier: "OU",
+	},
+	godkern: {
+		tier: "OU",
+	},
+	colorchanger: {
+		tier: "OU",
+	},
+	blisslax: {
+		tier: "OU",
+	},
+};
